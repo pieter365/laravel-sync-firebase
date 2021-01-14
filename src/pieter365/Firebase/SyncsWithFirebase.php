810e73658f809577354c1b9aec1a35dc0f2ca129
pieterp365 <pieterp365@gmail.com>
2021-01-14 22:56:49 +0000

    Laravel and Firebase

---
+++ src/pieter365/Firebase/SyncsWithFirebase.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Mpociot\Firebase;
+
+use Firebase\FirebaseInterface;
+use Firebase\FirebaseLib;
+
+/**
+ * Class SyncsWithFirebase
+ * @package App\Traits
+ */
+trait SyncsWithFirebase
+{
+
+    /**
+     * @var FirebaseInterface|null
+     */
+    protected $firebaseClient;
+
+    /**
+     * Boot the trait and add the model events to synchronize with firebase
+     */
+    public static function bootSyncsWithFirebase()
+    {
+        static::created(function ($model) {
+            $model->saveToFirebase('set');
+        });
+        static::updated(function ($model) {
+            $model->saveToFirebase('update');
+        });
+        static::deleted(function ($model) {
+            $model->saveToFirebase('delete');
+        });
+        static::restored(function ($model) {
+            $model->saveToFirebase('set');
+        });
+    }
+
+    /**
+     * @param FirebaseInterface|null $firebaseClient
+     */
+    public function setFirebaseClient($firebaseClient)
+    {
+        $this->firebaseClient = $firebaseClient;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getFirebaseSyncData()
+    {
+        if ($fresh = $this->fresh()) {
+            return $fresh->toArray();
+        }
+        return [];
+    }
+
+    /**
+     * @param $mode
+     */
+    protected function saveToFirebase($mode)
+    {
+        if (is_null($this->firebaseClient)) {
+            $this->firebaseClient = new FirebaseLib(config('services.firebase.database_url'), config('services.firebase.secret'));
+        }
+        $path = $this->getTable() . '/' . $this->getKey();
+
+        if ($mode === 'set') {
+            $this->firebaseClient->set($path, $this->getFirebaseSyncData());
+        } elseif ($mode === 'update') {
+            $this->firebaseClient->update($path, $this->getFirebaseSyncData());
+        } elseif ($mode === 'delete') {
+            $this->firebaseClient->delete($path);
+        }
+    }
+}
